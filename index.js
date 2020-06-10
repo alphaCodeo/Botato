1cf68df5f9edc0cd87f21ecf392bc07de9e7597d
codeo <alphaCodeo.users.noreply@github.com>
2020-06-10 13:20:40 +0800

    organised on message callback if statements

--- index.js
+++ index.js
@@ -76,252 +76,165 @@ function image(message){
 }
 
 // Command: Reply to messages
-
-
-
-
 bot.on('message', msg=>{
     if(msg.content ===  "if htis woirks i am gonna cry"){
         msg.reply('bot not works'); 
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content ===  "bot not works"){
         msg.reply('if htis woirks i am gonna cry'); 
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content ===  "bot you are gay"){
         msg.reply('how about you go and kill yourself u idot'); 
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content ===  "is Jai gay?"){
         msg.reply('ofc he is why even ask'); 
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content ===  "very toxic"){
         msg.reply('at least i dont say the n word'); 
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content ===  "who said the n word?"){
         msg.reply('ninja said the n word'); 
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content ===  "info"){
         msg.reply('i am behind you right now (version 1.0.8)'); 
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content ===  "how kind am i?"){
         msg.reply('error kindness not found pls kys'); 
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content ===  "kys"){
         msg.reply('pls be kind to your peers you peice of shit'); 
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('anime')) {
         msg.reply('go away you weeb');
     }
-})
 
-
-bot.on('message', msg=>{
     if(msg.content.includes('my')) {
         msg.reply('Our');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('kys')) {
         msg.reply('i dont think saying kill yourself is very nice would you want to correct your statement?');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('idk')) {
         msg.reply('dont use short forms it wont help you improve your english');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('idc')) {
         msg.reply('dont use short forms it wont help you improve your english');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('smh')) {
         msg.reply('dont use short forms it wont help you improve your english');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('jai')) {
         msg.reply('did someone say Jai?');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('owen')) {
         msg.reply('dont you mean OwOwen');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('gay')) {
         msg.reply('i dont think it is very nice to say that');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('ak ')) {
         msg.reply('The AK-47, officially known as the Avtomat Kalashnikova (Russian: Автома́т Кала́шникова, tr. Avtomát Kaláshnikova, lit. Kalashnikov’s automatic device also known as the Kalashnikov and AK, is a gas-operated, 7.62×39mm assault rifle, developed in the Soviet Union by Mikhail Kalashnikov. It is the originating firearm of the Kalashnikov rifle (or "AK") family. 47 refers to the year it was finished.');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('U ')) {
         msg.reply('Not even full word');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('lmao')) {
         msg.reply('saying lmao does not make something funny');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('owo')) {
         msg.reply('go away furry');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('uwu')) {
         msg.reply('go away furry');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('dont give claire admin')) {
         msg.reply('dont give claire admin');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('dont give claire admin')) {
         msg.reply('dont give claire admin');
     }
-})
 
-
-// Command: Spam something
-
-bot.on('message', msg=>{
+    // Command: Spam something
     if(msg.content.includes('hit or miss')) {
         msg.reply('hit or miss');
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('somethinbg')) {
         msg.reply('sometrhh');
     }
-})
-
-// Command: Images
-
-
-bot.on('message', msg=>{
+	//
+    // Command: Images
     if(msg.content.includes('my')) {
         msg.reply('i dont think that is very communist of you, you will now be sent to the Gulags. GLHF', {files: ['./images/commie.png']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('hitler')) {
         msg.reply('That sounds abit nazi like', {files: ['./images/nazi.png']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('nazi')) {
         msg.reply('That sounds abit Nazi like', {files: ['./images/nazi.png']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('kkk')) {
         msg.reply('Thats abit racist', {files: ['./images/kkk.png']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('I')) {
         msg.reply('i dont think that is very communist of you, you will now be sent to the Gulags. GLHF', {files: ['./images/commie.png']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('mine')) {
         msg.reply('i dont think that is very communist of you, you will now be sent to the Gulags. GLHF', {files: ['./images/commie.png']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('me')) {
         msg.reply('i dont think that is very communist of you, you will now be sent to the Gulags. GLHF', {files: ['./images/commie.png']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('our')) {
         msg.reply('Good job comrade', {files: ['./images/commie.png']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('ours')) {
         msg.reply('Good job comrade', {files: ['./images/commie.png']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('salakau')) {
         msg.reply('join this gang it is fun', {files: ['./images/sala.jpg']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('allahhu')) {
         msg.reply('kobe', {files: ['./images/boomer.jpg']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('allah')) {
         msg.reply('kobe', {files: ['./images/boomer.jpg']});
     }
-})
 
-bot.on('message', msg=>{
     if(msg.content.includes('akbar')) {
         msg.reply('kobe', {files: ['./images/boomer.jpg']});
     }
